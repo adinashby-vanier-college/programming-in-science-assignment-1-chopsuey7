8cf49aee0769930ef30e5627b6e473e049f7d57c
chopsuey7 <noahesar@gmail.com>
2025-03-10 16:19:52 +0000

    final trial

--- Assignment1.py
+++ Assignment1.py
@@ -1,12 +1,12 @@
 # Function 1: Using Python built-in functions
 # This function should take three numbers as input and return their max.
 def built_in_functions_max(num1, num2, num3):
-    return (num1,num2,num3)
+    return max(num1,num2,num3)
 
 # Function 2: Using Python built-in functions
 # This function should take three numbers as input and return their min.
 def built_in_functions_min(num1, num2, num3):
-    return (num1,num2,num3)
+    return min(num1,num2,num3)
 
 # Function 3: Conditional Statements – The If Statement
 # This function should check if a number is positive, negative, or zero and return the corresponding message.
